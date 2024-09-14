cc65322d7bb40b3737aef7b2787e30f24b249d91
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-09-15 00:07:06 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,122 +1,147 @@
--- Dollarware example script
-
--- Snag the ui loader function thingy (loadstring the link, but dont call it)
-local uiLoader = loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/dollarware/main/library.lua'))
--- Because of the way the library loads, settings are handled on the loadstring call
-local ui = uiLoader({
-    rounding = true, -- Whether certain features get rounded 
-    theme = 'cherry', -- The theme. Available themes are: cherry, orange, lemon, lime, raspberry, blueberry, grape, watermelon
-    smoothDragging = true, -- Smooth dragging
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() 
+local Window = OrionLib:MakeWindow({Name = "Qwerty hub", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
-ui.autoDisableToggles = true -- All toggles will automatically be disabled when the ui is destroyed (window is closed)
--- so you don't have to manually handle everything. This defaults to true!
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
 
--- Make a window, which houses all the stuff for the gui
--- Technically multiple windows can be made, but there is no (and likely wont ever be) official support for them
--- since its a lot of work for such a minute use
-local window = ui.newWindow({
-    text = 'Dollarware demo', -- Title of window 
-    resize = true, -- Ability to resize
-    size = Vector2.new(550, 376), -- Window size, accepts UDim2s and Vector2s
-    position = nil -- Custom position, defaults to roughly the bottom right corner
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
 })
 
-local menu = window:addMenu({
-    text = 'menu 1' -- Title of menu
-})
-do 
-    -- Menus have sections which house all the controls    
-    local section = menu:addSection({
-        text = 'section 1', -- Title of section
-        side = 'auto', -- Side of the menu that the section is placed on. Defaults to 'auto', but can be 'left' or 'right'
-        showMinButton = true, -- Ability to minimize this section. Defaults to true
-    })
-    
-    do 
-        section:addLabel({
-            text = 'text' -- Self explanatory
-        })
-        
-        local toggle = section:addToggle({
-            text = 'toggle', 
-            state = false -- Starting state of the toggle - doesn't automatically call the callback
-        })
-        
-        toggle:bindToEvent('onToggle', function(newState) -- Call a function when toggled
-            ui.notify({
-                title = 'toggle',
-                message = 'Toggle was toggled to ' .. tostring(newState),
-                duration = 3
-            })
-        end)
-        
-        section:addButton({
-            text = 'button (small)', 
-            style = 'small' -- style of the button, can be 'large' or 'small'
-        }):bindToEvent('onClick', function() -- Call a function when clicked
-            ui.notify({
-                title = 'button',
-                message = 'The button got clicked!',
-                duration = 3
-            })
-        end)
-        
-        section:addButton({
-            text = 'button (large)', 
-            style = 'large' -- style of the button, can be 'large' or 'small'
-        }, function() -- you don't have to always use bindToEvent, just passing a callback normally works fine
-            ui.notify({
-                title = 'button',
-                message = 'The large button got clicked!',
-                duration = 3
-            })
-        end):setTooltip('this is a large button')
-        
-        local hotkey = section:addHotkey({
-            text = 'hotkey'
-        })
-        hotkey:setHotkey(Enum.KeyCode.G)
-        hotkey:setTooltip('This is a hotkey linked to the toggle!')
-        hotkey:linkToControl(toggle)
-    end
-    
-    local section = menu:addSection({
-        text = 'section 2',
-        side = 'right',
-        showMinButton = false
-    })
-    do 
-        section:addSlider({
-            text = 'slider',
-            min = 1,
-            max = 150,
-            step = 0.01,
-            val = 50
-        }, function(newValue) 
-            print(newValue)
-        end):setTooltip('Heres a slider!')
-        
-        section:addColorPicker({
-            text = 'color picker',
-            color = Color3.fromRGB(255, 0, 0)
-        }, function(newColor) 
-            print(newColor)
-        end)
-        
-        section:addTextbox({
-            text = 'textbox'
-        }):bindToEvent('onFocusLost', function(text) 
-            ui.notify({
-                title = 'textbox',
-                message = text,
-                duration = 4
-            })
-        end)
-    end
-    
-end
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
 
-window:addMenu({
-    text = 'menu 2'
-})!
+Tab:AddToggle({
+	Name = "This is a toggle!",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+-- CoolToggle:Set(true)  
+
+--[[
+Name = <string> - The name of the toggle.
+Default = <bool> - The default value of the toggle.
+Callback = <function> - The function of the toggle.
+]]
+
+Tab:AddColorpicker({
+	Name = "Colorpicker",
+	Default = Color3.fromRGB(255, 0, 0),
+	Callback = function(Value)
+		print(Value)
+	end	  
+})
+
+--[[
+Name = <string> - The name of the colorpicker.
+Default = <color3> - The default value of the colorpicker.
+Callback = <function> - The function of the colorpicker.
+]]
+Tab:AddSlider({
+	Name = "Slider",
+	Min = 16,
+	Max = 100,
+	Default = 16,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "bananas",
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+Slider:Set(18)
+
+--[[
+Name = <string> - The name of the slider.
+Min = <number> - The minimal value of the slider.
+Max = <number> - The maxium value of the slider.
+Increment = <number> - How much the slider will change value when dragging.
+Default = <number> - The default value of the slider.
+ValueName = <string> - The text after the value number.
+Callback = <function> - The function of the slider.
+]]
+Tab:AddLabel("Label")
+CoolLabel:Set("Label New!")
+Tab:AddParagraph("Paragraph","Paragraph Content")
+CoolParagraph:Set("Paragraph New!", "New Paragraph Content!")
+Tab:AddTextbox({
+	Name = "Textbox",
+	Default = "default box input",
+	TextDisappear = true,
+	Callback = function(Value)
+		print(Value)
+	end	  
+})
+
+--[[
+Name = <string> - The name of the textbox.
+Default = <string> - The default value of the textbox.
+TextDisappear = <bool> - Makes the text disappear in the textbox after losing focus.
+Callback = <function> - The function of the textbox.
+]]
+Tab:AddBind({
+	Name = "Bind",
+	Default = Enum.KeyCode.E,
+	Hold = false,
+	Callback = function()
+		print("press")
+	end    
+})
+
+--[[
+Name = <string> - The name of the bind.
+Default = <keycode> - The default value of the bind.
+Hold = <bool> - Makes the bind work like: Holding the key > The bind returns true, Not holding the key > Bind returns false.
+Callback = <function> - The function of the bind.
+]]
+Bind:Set(Enum.KeyCode.E)
+Tab:AddDropdown({
+	Name = "Dropdown",
+	Default = "1",
+	Options = {"1", "2"},
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+--[[
+Name = <string> - The name of the dropdown.
+Default = <string> - The default value of the dropdown.
+Options = <table> - The options in the dropdown.
+Callback = <function> - The function of the dropdown.
+]]
+OrionLib:Init()
+--[[ Tab1:AddToggle({
+    Name = "Toggle",
+    Default = true,
+    Save = true,
+    Flag = "toggle"
+})
+
+print(OrionLib.Flags["toggle"].Value) -- prints the value of the toggle. ]]-- 
